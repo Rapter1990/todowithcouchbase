4d8ca30c91479062079f6a3a802ab630fb9b492b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-09-22 14:54:05 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/todowithcouchbase/TodowithcouchbaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.todowithcouchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodowithcouchbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodowithcouchbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todowithcouchbase/TodowithcouchbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todowithcouchbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodowithcouchbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
