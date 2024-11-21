0d4d42383103e6100276612e2b22dd16726451b6
harunyusuf <yusuf.eksioglu@payco.com.tr>
2024-11-21 09:46:54 +0300

    construct controller,mapper,Service,Request and ResponseObject

---
+++ src/main/java/com/example/todowithcouchbase/task/model/mapper/task/SaveTaskRequestToTaskEntityMapper.java
@@ -0,0 +1,24 @@
+package com.example.todowithcouchbase.task.model.mapper.task;
+
+import com.example.todowithcouchbase.common.model.mapper.BaseMapper;
+import com.example.todowithcouchbase.task.model.dto.request.SaveTaskRequest;
+import com.example.todowithcouchbase.task.model.entity.TaskEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface SaveTaskRequestToTaskEntityMapper extends BaseMapper<SaveTaskRequest, TaskEntity> {
+
+    @Named("mapForSaving")
+    default TaskEntity mapForSaving(SaveTaskRequest request){
+        return TaskEntity.builder()
+                .name(request.getName())
+                .build();
+    }
+
+    static SaveTaskRequestToTaskEntityMapper initialize(){
+        return Mappers.getMapper(SaveTaskRequestToTaskEntityMapper.class);
+    }
+
+}
---
+++ src/main/java/com/example/todowithcouchbase/task/model/mapper/task/TaskToTaskResponseMapper.java
@@ -0,0 +1,15 @@
+package com.example.todowithcouchbase.task.model.mapper.task;
+
+import com.example.todowithcouchbase.common.model.mapper.BaseMapper;
+import com.example.todowithcouchbase.task.model.Task;
+import com.example.todowithcouchbase.task.model.dto.response.TaskResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface TaskToTaskResponseMapper extends BaseMapper<Task, TaskResponse> {
+
+    static TaskToTaskResponseMapper initialize() {
+        return Mappers.getMapper(TaskToTaskResponseMapper.class);
+    }
+}
