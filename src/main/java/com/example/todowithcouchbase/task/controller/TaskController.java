ee26e572bb8572c41cc40206f3c768bafbf6d845
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-11-21 11:27:13 +0300

    Revise TaskController

--- src/main/java/com/example/todowithcouchbase/task/controller/TaskController.java
+++ src/main/java/com/example/todowithcouchbase/task/controller/TaskController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/task")
+@RequestMapping("api/v1/tasks")
 @Validated
 public class TaskController {
 
