a54564b434a5f9ef2fe27c8a29fdef8e7c19a9fe
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-11-23 21:32:27 +0300

    Revise LoggerAspectJ,pom.xml, TaskNotFoundException and TaskWithThisNameAlreadyExistException

--- src/main/java/com/example/todowithcouchbase/task/exception/TaskNotFoundException.java
+++ src/main/java/com/example/todowithcouchbase/task/exception/TaskNotFoundException.java
@@ -1,11 +1,15 @@
 package com.example.todowithcouchbase.task.exception;
 
+import org.springframework.http.HttpStatus;
+
 import java.io.Serial;
 
 public class TaskNotFoundException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = 7222351960801029610L;
+
+    public static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
 
     private static final String DEFAULT_MESSAGE = """
             Task not found!
--- src/main/java/com/example/todowithcouchbase/task/exception/TaskWithThisNameAlreadyExistException.java
+++ src/main/java/com/example/todowithcouchbase/task/exception/TaskWithThisNameAlreadyExistException.java
@@ -1,11 +1,15 @@
 package com.example.todowithcouchbase.task.exception;
 
+import org.springframework.http.HttpStatus;
+
 import java.io.Serial;
 
 public class TaskWithThisNameAlreadyExistException extends RuntimeException{
 
     @Serial
     private static final long serialVersionUID = -3495060754159297663L;
+
+    public static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
 
     private static final String DEFAULT_MESSAGE = """
             Task with this name already exist
