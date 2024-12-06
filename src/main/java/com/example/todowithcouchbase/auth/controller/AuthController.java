a7487e1fa88c55921bfd9f4408ff2e8a39b54338
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-12-07 00:11:27 +0300

    Add OpenApi Documentation for AuthController and TaskController

--- src/main/java/com/example/todowithcouchbase/auth/controller/AuthController.java
+++ src/main/java/com/example/todowithcouchbase/auth/controller/AuthController.java
@@ -12,6 +12,9 @@ import com.example.todowithcouchbase.auth.service.LogoutService;
 import com.example.todowithcouchbase.auth.service.RefreshTokenService;
 import com.example.todowithcouchbase.auth.service.RegisterService;
 import com.example.todowithcouchbase.common.model.dto.response.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -30,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/authentication/user")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Authentication", description = "Handles user authentication and authorization operations.")
 public class AuthController {
 
     private final RegisterService registerService;
@@ -48,6 +52,15 @@ public class AuthController {
      * @param registerRequest The {@link RegisterRequest} object containing user registration details.
      * @return A {@link CustomResponse} indicating the success of the registration operation.
      */
+    @Operation(
+            summary = "Register a new user",
+            description = "Registers a new user in the system.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User successfully registered"),
+                    @ApiResponse(responseCode = "400", description = "Invalid request data"),
+                    @ApiResponse(responseCode = "409", description = "User already exists")
+            }
+    )
     @PostMapping("/register")
     public CustomResponse<Void> registerAdmin(@RequestBody @Valid final RegisterRequest registerRequest) {
         registerService.registerUser(registerRequest);
@@ -60,6 +73,14 @@ public class AuthController {
      * @param loginRequest The {@link LoginRequest} object containing user login credentials.
      * @return A {@link CustomResponse} containing a {@link TokenResponse} with the generated tokens.
      */
+    @Operation(
+            summary = "User login",
+            description = "Authenticates a user and returns an access and refresh token.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Login successful"),
+                    @ApiResponse(responseCode = "401", description = "Invalid login credentials")
+            }
+    )
     @PostMapping("/login")
     public CustomResponse<TokenResponse> loginAdmin(@RequestBody @Valid final LoginRequest loginRequest) {
         final Token token = loginService.login(loginRequest);
@@ -73,6 +94,15 @@ public class AuthController {
      * @param tokenRefreshRequest The {@link TokenRefreshRequest} object containing the refresh token.
      * @return A {@link CustomResponse} containing a {@link TokenResponse} with the refreshed tokens.
      */
+    @Operation(
+            summary = "Refresh access token",
+            description = "Refreshes an expired access token using a valid refresh token.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Token successfully refreshed"),
+                    @ApiResponse(responseCode = "400", description = "Invalid refresh token"),
+                    @ApiResponse(responseCode = "401", description = "Refresh token expired or invalid")
+            }
+    )
     @PostMapping("/refresh-token")
     public CustomResponse<TokenResponse> refreshToken(@RequestBody @Valid final TokenRefreshRequest tokenRefreshRequest) {
         final Token token = refreshTokenService.refreshToken(tokenRefreshRequest);
@@ -86,6 +116,14 @@ public class AuthController {
      * @param tokenInvalidateRequest The {@link TokenInvalidateRequest} object containing the token to be invalidated.
      * @return A {@link CustomResponse} indicating the success of the logout operation.
      */
+    @Operation(
+            summary = "Log out a user",
+            description = "Invalidates the provided token, effectively logging out the user.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Logout successful"),
+                    @ApiResponse(responseCode = "400", description = "Invalid token provided")
+            }
+    )
     @PostMapping("/logout")
     public CustomResponse<Void> logout(@RequestBody @Valid final TokenInvalidateRequest tokenInvalidateRequest) {
         logoutService.logout(tokenInvalidateRequest);
--- src/main/java/com/example/todowithcouchbase/task/controller/TaskController.java
+++ src/main/java/com/example/todowithcouchbase/task/controller/TaskController.java
@@ -12,6 +12,9 @@ import com.example.todowithcouchbase.task.model.dto.response.TaskResponse;
 import com.example.todowithcouchbase.task.model.mapper.CustomPageTaskToCustomPagingTaskResponseMapper;
 import com.example.todowithcouchbase.task.model.mapper.TaskToTaskResponseMapper;
 import com.example.todowithcouchbase.task.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.UUID;
@@ -27,6 +30,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/tasks")
 @Validated
+@Tag(name = "Task Management", description = "Endpoints for managing tasks including creation, retrieval, updating, and deletion.")
 public class TaskController {
 
     private final TaskService taskService;
@@ -41,6 +45,16 @@ public class TaskController {
      * @param saveTaskRequest the request body containing the task details to be created.
      * @return a response containing the ID of the created task.
      */
+    @Operation(
+            summary = "Create a new task",
+            description = "Creates a new task and saves it to the database. Accessible by ADMIN only.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Task successfully created"),
+                    @ApiResponse(responseCode = "400", description = "Invalid task details provided"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized, authentication is required"),
+                    @ApiResponse(responseCode = "403", description = "Access forbidden")
+            }
+    )
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public CustomResponse<String> saveTask(@RequestBody @Valid final SaveTaskRequest saveTaskRequest){
@@ -55,6 +69,17 @@ public class TaskController {
      * @param request the request body containing pagination and sorting information.
      * @return a paginated response containing a list of tasks.
      */
+    @Operation(
+            summary = "Get all tasks",
+            description = "Retrieves a paginated list of tasks. Accessible by both ADMIN and USER roles.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Tasks successfully retrieved"),
+                    @ApiResponse(responseCode = "400", description = "Invalid update details provided"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized, authentication is required"),
+                    @ApiResponse(responseCode = "403", description = "Access forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Task not found")
+            }
+    )
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     public CustomResponse<CustomPagingResponse<TaskResponse>> getAllTasks(@RequestBody @Valid final TaskPagingRequest request){
@@ -72,6 +97,17 @@ public class TaskController {
      * @param getTaskByNameRequest the request body containing the task name to search for.
      * @return a response containing the task details.
      */
+    @Operation(
+            summary = "Get task by name",
+            description = "Retrieves a task by its name. Accessible by both ADMIN and USER roles.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Task successfully retrieved"),
+                    @ApiResponse(responseCode = "400", description = "Invalid update details provided"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized, authentication is required"),
+                    @ApiResponse(responseCode = "403", description = "Access forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Task not found")
+            }
+    )
     @PostMapping("/getByName")
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     public CustomResponse<TaskResponse> getTaskByName(@RequestBody @Valid final GetTaskByNameRequest getTaskByNameRequest){
@@ -88,6 +124,17 @@ public class TaskController {
      * @param id the ID of the task to be retrieved.
      * @return a response containing the task details.
      */
+    @Operation(
+            summary = "Get task by ID",
+            description = "Retrieves a task by its ID. Accessible by both ADMIN and USER roles.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Task successfully retrieved"),
+                    @ApiResponse(responseCode = "400", description = "Invalid update details provided"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized, authentication is required"),
+                    @ApiResponse(responseCode = "403", description = "Access forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Task not found")
+            }
+    )
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     public CustomResponse<TaskResponse> getTaskById(@PathVariable @Valid @UUID final String id){
@@ -105,6 +152,17 @@ public class TaskController {
      * @param updateTaskRequest the request body containing the updated task details.
      * @return a response containing the updated task details.
      */
+    @Operation(
+            summary = "Update a task",
+            description = "Updates an existing task by its ID. Accessible by ADMIN only.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Task successfully updated"),
+                    @ApiResponse(responseCode = "400", description = "Invalid update details provided"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized, authentication is required"),
+                    @ApiResponse(responseCode = "403", description = "Access forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Task not found")
+            }
+    )
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public CustomResponse<TaskResponse> updateTaskById(
@@ -125,6 +183,17 @@ public class TaskController {
      * @param id the ID of the task to be deleted.
      * @return a response containing a success message.
      */
+    @Operation(
+            summary = "Delete a task",
+            description = "Deletes a task by its ID. Accessible by ADMIN only.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Task successfully deleted"),
+                    @ApiResponse(responseCode = "400", description = "Invalid update details provided"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized, authentication is required"),
+                    @ApiResponse(responseCode = "403", description = "Access forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Task not found")
+            }
+    )
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public CustomResponse<String> deleteTaskById(@PathVariable @Valid final String id){
@@ -136,5 +205,3 @@ public class TaskController {
     }
 
 }
-
-
