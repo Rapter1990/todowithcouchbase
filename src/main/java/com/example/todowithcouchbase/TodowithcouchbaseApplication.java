34afd6c797bcc5183e5fdf33b07e7fe6f228e10d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-12-06 23:12:21 +0300

    Add Java Doc for TodowithcouchbaseApplication

--- src/main/java/com/example/todowithcouchbase/TodowithcouchbaseApplication.java
+++ src/main/java/com/example/todowithcouchbase/TodowithcouchbaseApplication.java
@@ -3,9 +3,23 @@ package com.example.todowithcouchbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point for the Todo application with Couchbase integration.
+ * This class is responsible for launching the Spring Boot application, which initializes all necessary components,
+ * including configurations for Couchbase, and starts the application context.
+ * The {@link SpringApplication#run(Class, String...)} method is invoked to start the application.
+ */
 @SpringBootApplication
 public class TodowithcouchbaseApplication {
 
+	/**
+	 * The main method which serves as the entry point to launch the Spring Boot application.
+	 * This method initializes and runs the {@link SpringApplication} to start the application.
+	 * It automatically configures the application context and any required components.
+	 *
+	 * @param args Command-line arguments passed to the application.
+	 *             These are forwarded to the {@link SpringApplication#run(Class, String...)} method.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TodowithcouchbaseApplication.class, args);
 	}
